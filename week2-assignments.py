e4c9830ad17e426a1dd619d5409cc615d62df064
richie <richardkanyira@gmail.com>
2025-03-31 17:43:48 +0300

    Create week2-assignments.py

---
+++ week2-assignments.py
@@ -0,0 +1,22 @@
+# enpty list
+my_list = []
+# appending list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+# adding value to second position
+my_list.insert(1, 15)
+
+# Extend my_list with another list
+my_list.extend([50, 60, 70])
+
+# Remove the last element
+my_list.pop()
+
+# Sort the list in ascending order
+my_list.sort()
+
+# Find and print the index of value 30
+index_30 = my_list.index(30)
+print("Index of 30:", index_30)
